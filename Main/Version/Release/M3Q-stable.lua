5596f7b26c86233263a4c0c0702a2804ea7efe85
ScadeBlock <89845150+ScadeBlock@users.noreply.github.com>
2023-03-01 19:47:48 +0700

    Update M3Q-stable.lua

--- Main/Version/Release/M3Q-stable.lua
+++ Main/Version/Release/M3Q-stable.lua
@@ -1,15 +1,55 @@
-print("M3Q Version 1.0.0 (Release Date: 02.25.23) by DatCanFly")
-local uis = game:GetService("UserInputService")
-local MAXFPS = 30 -- MAX FPS WHEN SCREEN IS ACTIVE (30 IS DEFAULT)
-local FREEZEFPS = 1 -- FPS WHEN SCREEN IS UNACTIVE - MINIMIZE (UNDER 10 FPS IS HIGHLY RECOMMEND)
--- NOTE : 1 IS THE BEST BUT I WILL TAKE 1 - 3 SECONDS TO REACTIVE
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("M3Q 1.0.2 - STABLE [DATCANFLY]", "Ocean")
+local MAXFPS = 30 
+local FREEZEFPS = 1 
 setfpscap(MAXFPS)
-uis.WindowFocused:Connect(function() 
+local Main = Window:NewTab("Main")
+local MainSection = Main:NewSection("Main")
+MainSection:NewToggle("Dynamic Memory", "Reduce fps when the mainscreen is unactive", function(state)
+    if state then
+        local uis = game:GetService("UserInputService")
+        setfpscap(MAXFPS)
+        connectru = uis.WindowFocused:Connect(function() 
+            setfpscap(MAXFPS)
+        end)
+        conntecturnu = uis.WindowFocusReleased:Connect(function() 
+            setfpscap(FREEZEFPS)
+        end)
+    else
+        setfpscap(MAXFPS)
+        connectru:Disconnect()
+        connectru= nil
+        conntecturnu:Disconnect()
+        conntecturnu = nil
+    end
+end)
+local Player = Window:NewTab("Config")
+local PlayerSection = Player:NewSection("Config")
+PlayerSection:NewSlider("Max FPS", "ANY", 600, 15, function(s)
+    MAXFPS = s
     setfpscap(MAXFPS)
 end)
-
-uis.WindowFocusReleased:Connect(function() 
-    setfpscap(FREEZEFPS)
+PlayerSection:NewSlider("FREEZE FPS", "FPS when your screen is freezing", 50, 1, function(s)
+    FREEZEFPS = s
 end)
-
--- Add new code HERE
+local Other = Window:NewTab("Other")
+local OtherSection = Other:NewSection("Other")
+OtherSection:NewToggle("Super-Human", "go fast and jump high", function(state)
+    if state then
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 120
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = 120
+    else
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
+    end
+end)
+OtherSection:NewSlider("Walkspeed", "SPEED!!", 500, 16, function(s)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+OtherSection:NewSlider("Jumppower", "JUMP HIGH!!", 350, 50, function(s)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
+OtherSection:NewButton("Reset WS/JP", "Resets to all defaults", function()
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+end)
